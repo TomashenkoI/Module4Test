599df1d5e41d7b2b21039c6a37c0af88a8b6e81b
snitelol <TomashenkoI@gmail.com>
2016-04-06 16:53:30 +0300

    v1

--- src/ua/goit/java/RectangleSquareCalculating.java
+++ src/ua/goit/java/RectangleSquareCalculating.java
@@ -4,13 +4,13 @@ package ua.goit.java;
  * Created by 7 on 04.04.2016.
  */
 public class RectangleSquareCalculating {
-      public static String rectangleSquareCalculating(String SideAOfRectangle, String SideBOfRectangle) {
+      public static double rectangleSquareCalculating(String SideAOfRectangle, String SideBOfRectangle) {
           final int IntSideAOfRectangle = Integer.parseInt(SideAOfRectangle);
           final int IntSideBOfRectangle = Integer.parseInt(SideBOfRectangle);
           double RectangleSquare;
           RectangleSquare = IntSideAOfRectangle*IntSideBOfRectangle;
           System.out.println("Square Area is = "+RectangleSquare);
-          return null;
+          return RectangleSquare;
       }
 
 }
